833338389cdcccee69d8a073f37b9cfe780834dc
rodney341 <huilyrg@gmail.com>
2023-08-08 16:51:51 -0500

    Agregando cambios a la clase complejos

--- complejos/src/main/java/com/example/complejos/Principal.java
+++ complejos/src/main/java/com/example/complejos/Principal.java
@@ -3,7 +3,7 @@ package com.example.complejos;
 public class Principal {
 
     public static void main(String[] main) {
-        ComplejoAmpliado z, w;
+        ComplejoAmpliado z, w,test;
         z = new ComplejoAmpliado(-1.5, 0.0);
         w = new ComplejoAmpliado(-1.2, 0.0);
         z.suma(w);
